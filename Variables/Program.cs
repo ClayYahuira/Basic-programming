ee1f188a37e122c514a626b9960cb36a8be23456
Carlin Yahuira Achahui <clay.achahui@gmail.com>
2026-07-22 19:52:51 +0200

    what is variable

---
+++ Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿// Elegimos el tipo de variable que será y le damos un nombre acorde a lo que va a almacenar.
+string name;
+
+// Ahora le podemos asignar un valor a nuestra variable name.
+name = "carlin";
+
+// Esta linea hará que el valor de la variable se muestre en la consola.
+Console.WriteLine(name);
